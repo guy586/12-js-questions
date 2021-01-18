26a19fb5891956ccf430f05e70dc16d9991083f6
guy586 <guy586@gmail.com>
2021-01-18 13:53:08 +0200

    Create q1.js
    Solution to q1

---
+++ q1.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("Enter the first number"));
+let num2 = Number(prompt("Enter the second number"));
+if (num1 > num2) {
+    alert("The larger number is " + num1);
+}
+alert("The larger number is " + num2);
